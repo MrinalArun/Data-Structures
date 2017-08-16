fe37e304ce0da0830ee3900a26322121de317ca7
MrinalArun <mrinalarun@gmail.com>
2017-08-16 19:03:51 -0400

    Create LinearSearch.cpp

---
+++ LinearSearch.cpp
@@ -0,0 +1,11 @@
+Linear Search (The time complexity of above algorithm is O(n).)
+// Linearly search x in arr[].  If x is present then return its 
+// location,  otherwise return -1
+int search(int arr[], int n, int x)
+{
+    int i;
+    for (i=0; i<n; i++)
+        if (arr[i] == x)
+         return i;
+    return -1;
+}
